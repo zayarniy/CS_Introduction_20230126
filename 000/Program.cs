6157d9e179c6a16d2104ced151bc634bd8841b54
Ivanov Ivan <ivanov@gmail.com>
2023-01-26 22:12:59 +0300

    Lesson1 01

---
+++ 000/Program.cs
@@ -0,0 +1,28 @@
+﻿// С клавиатуры вводятся два числа. Найти и вывести на экран их сумму
+//string s;
+int a,b,sum;
+System.Console.Write("Введите a:");
+a=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите b:");
+b=Convert.ToInt32(Console.ReadLine());
+sum=a+b;
+//1. Конкатенация (скливание) строк
+System.Console.WriteLine(a+"+"+b.ToString()+"="+Convert.ToString(sum));
+//2. Форматированный вывод
+System.Console.WriteLine("{0:F2}+{1:E5}={2:G4}",a,b,sum);
+//3. Интерполяция строк
+System.Console.WriteLine($"{a}+{b}={sum}");
+
+// С клавиатуры вводятся два числа. Найти и вывести на экран максимальное из них
+int max;
+if (a>b)
+{
+  max=a;
+}
+else
+{
+  max=b;
+}
+System.Console.WriteLine($"max={max}");
+
+
