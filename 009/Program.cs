ebb8eafa325698411e2d6e6bf4e6a737f5485471
Ivanov Ivan <ivanov@gmail.com>
2023-01-26 22:35:15 +0300

    Lesson1 02

---
+++ 009/Program.cs
@@ -0,0 +1,12 @@
+﻿// 9. Вывести на экран четные числа от 1 до N
+int i=2;
+int N=10;
+N=Convert.ToInt32(Console.ReadLine());
+//нц пока i<=N
+//кц
+while (i<=N)
+{
+    System.Console.WriteLine(i);   
+    i=i+2;
+}
+
---
+++ 011/Program.cs
@@ -0,0 +1,21 @@
+﻿//11. Дано число больше 9. Вывести на экран  вторую цифру числа с конца
+
+int n=123;
+int d0,d1,d2;
+d0=n%10;
+d1=n/10%10;
+d2=n/100;
+System.Console.WriteLine($"{d0} {d1} {d2}");
+System.Console.WriteLine($"{d1}");
+
+//7. Выяснить является ли число чётным.
+int N2=10;
+bool odd= N2%2==0;
+if (odd)
+{
+    System.Console.WriteLine($"Число {N2} четное");
+}
+else
+{
+    System.Console.WriteLine($"Число {N2} не четное");
+}
