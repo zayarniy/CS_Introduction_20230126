8494444710bd12a6bd3b97ace6fbc05695c64a48
Ivanov Ivan <ivanov@gmail.com>
2023-01-30 22:31:42 +0300

    Lesson 2_1

---
+++ 013/Program.cs
@@ -0,0 +1,15 @@
+﻿//13. Удалить вторую цифру целого числа введенного с клавиатуры.
+
+int N=1245;
+int counterDigit=0;
+int N1=N;
+while(N!=0)
+{
+    counterDigit++;
+    N/=10;
+}
+int d=N1/(int)Math.Pow(10,counterDigit-3)%10;
+System.Console.WriteLine(d);
+int d1=N1/(int)Math.Pow(10,counterDigit-1)%10;
+int n2=N1%(int)Math.Pow(10,counterDigit-2);
+System.Console.WriteLine(d1*Math.Pow(10,counterDigit-2)+n2);
