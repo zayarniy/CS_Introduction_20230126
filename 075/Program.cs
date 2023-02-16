6cd592af3866b4da69ee8580d16a9c8e74cd4d75
Ivanov Ivan <ivanov@gmail.com>
2023-02-16 21:43:54 +0300

    Lesson 7

---
+++ 075/Program.cs
@@ -0,0 +1,15 @@
+﻿//75. Написать программу вычисления функции Аккермана - 5*
+
+int A(int m, int n)
+{
+    if (m==0) return n+1;
+    else
+      if (m>0 && n==0) return A(m-1,1);
+        else
+         //if (m>0 && n>0) 
+         return A(m-1, A(m,n-1));
+}
+
+for(int m=0;m<4;m++)
+    for(int n=0;n<5;n++)
+        System.Console.WriteLine($"A({m},{n})={A(m,n)}");
---
+++ BasicAlgorithms/Recursy/Program.cs
@@ -0,0 +1,59 @@
+﻿//Вывести на экран числа от 1 до N без использования цикла. GOTO запрещен
+int N=20;
+
+int i=0;
+
+//System.Console.WriteLine(i++);//постинкримент
+// System.Console.WriteLine(++i);//прединкримент
+// System.Console.WriteLine(i);
+
+// while(i<N) 
+// {
+//     //i++;//инкремент
+//     System.Console.Write($"{i++,4}");
+// }
+
+//!!!!!!!!!!!ЗАПРЕЩЕНО!!!!!!!
+// metka:
+//   System.Console.Write($"{i++,4}");
+// if (i<N) goto metka;//оператор безусловного перехода
+
+//рекурсивный метод
+void Loop(int i, int N)
+{
+    System.Console.Write($"{i,4}");
+    if (i<N) Loop(i+1,N);//рекурсия
+}
+
+
+int F(int N)
+{
+    if (N>0) return F(N-1)*N;
+    else return 1;
+}
+
+int Fib(int N)
+{
+    if (N==0) return 0;
+        else
+            if (N==1) return 1;
+                else
+                    return Fib(N-1)+Fib(N-2);
+}
+
+double Power(int a,int b)
+{
+    if (b==0) return 1;
+    else 
+      if (b>0)
+        return Power(a,b-1)*a;
+      else
+        return Power(a,b+1)*1/a;
+
+}
+
+//Loop(1,20);
+//System.Console.WriteLine(F(5));
+for(i=-10;i!=0;i++)
+    //System.Console.WriteLine(Fib(i));
+    System.Console.WriteLine(Power(2,i));
